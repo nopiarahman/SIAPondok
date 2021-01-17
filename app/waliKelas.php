37d0149526eea730179679321a1d1019de27e4c3
nopiarahman <nopiarahman93@gmail.com>
2021-01-18 03:27:25 +0700

    model waliKelas

---
+++ app/waliKelas.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class waliKelas extends Model
+{
+    protected $table = 'waliKelas';
+    protected $guarded = ['id','created_at','updated_at']; /* melindungi field yang tidak boleh diisi manual, lihat mass assignment */
+
+    public function kelas()
+    {
+        return $this->belongsTo(kelas::class);
+    }
+
+}
