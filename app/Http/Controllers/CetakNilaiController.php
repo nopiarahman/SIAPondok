21859a3387e00b24df48bff8ce9cba9889b3744a
nopiarahman <nopiarahman93@gmail.com>
2021-04-09 11:20:45 +0700

    cetak uas uula

--- app/Http/Controllers/CetakNilaiController.php
+++ app/Http/Controllers/CetakNilaiController.php
@@ -33,10 +33,99 @@ class CetakNilaiController extends Controller
             $ceknilai = DB::table('nilai')
                     ->where('kelas_id', '=', $nilai->kelas_id)
                     ->get();
-            $cekaktif=['santriwustha_id'=> $santriwustha->id,'periode_id'=>$periode->id];
+            $cekaktif=['santriwustha_id'=> $santriwustha->id,'periode_id'=>$periode->id,'kelas_id'=>$santriwustha->kelas_id];
             $nilaiaktif=nilai::where($cekaktif)->get();
         }else{
             $nilaiaktif==null;
+        }
+        $jenjang=auth()->user()->jenjang;
+        if($jenjang='sd'){
+            $nilaidiniyah=[];
+            $nilaiumum=[];
+            $nilaidiniyahsorted=[];
+            $nilaiumumsorted=[];
+            $nilaiMulokSorted=[];
+            foreach($nilaiaktif as $nd)
+            {
+                if($nd->mapel->kategori=='diniyah')
+                {
+                    $nd['namaMapel']= $nd->mapel->namaMapel;
+                    $nilaidiniyah[]=$nd;
+                    $nilaidiniyahsorted = array_values(Arr::sort($nilaidiniyah, function ($value) {
+                        return $value['namaMapel'];
+                    }));
+                }
+                elseif($nd->mapel->kategori=='umum')
+                {
+                    // $nilaiumum[]=$nd;
+                    $nd['namaMapel']= $nd->mapel->namaMapel;
+                    $nilaiumum[]=$nd;
+                    $nilaiumumsorted = array_values(Arr::sort($nilaiumum, function ($value) {
+                        return $value['namaMapel'];
+                    }));
+                }else{
+                    $nd['namaMapel']= $nd->mapel->namaMapel;
+                    $nilaiMulok[]=$nd;
+                    $nilaiMulokSorted = array_values(Arr::sort($nilaiMulok, function ($value) {
+                        return $value['namaMapel'];
+                    }));
+                }
+            }
+            $cekuser = auth()->user();
+            $walikelas=waliKelas::where('user_id',$cekuser->id)->first();
+            // dd($periode->semester);
+            if($periode->semester=='Ganjil'){
+                $pdf = PDF::loadview('laporan/cetakUASGanjilUula',compact('santriwustha','nilaiaktif','tanggal','kelas','periode','nilaidiniyahsorted','nilaiumumsorted','nilaiMulokSorted','walikelas'));
+                $pdf->setPaper('legal');
+                return $pdf->stream('raport'.$santriwustha->namaLengkap.'.pdf');
+                return view('laporan/cetakUASGanjilUula',compact('santriwustha','nilaiaktif','tanggal','kelas','periode','nilaidiniyahsorted','nilaiumumsorted','nilaiMulokSorted','walikelas'));
+            }elseif($periode->semester=='Genap'){
+                /* Nilai/variabel yang nanti dikirim lewat form */
+                $statusNaik = "naik";
+                if($statusNaik=="naik"){
+                    if($kelas->namaKelas<=6){
+                        $naik = $kelas->namaKelas+1;
+                        $tidaknaik=false;
+                    }else{
+                        $naik = "Lulus";
+                        $tidaknaik="Tidak Lulus";
+                    }
+                }else{
+                    if($kelas->namaKelas<=6){
+                        $naik=$kelas->namaKelas;
+                        $tidaknaik=true;
+                    }else{
+                        $naik = "Lulus";
+                        $tidaknaik="Tidak Lulus";
+                    }
+                }
+                $pdf = PDF::loadview('laporan/cetakUASGenapUula',compact('naik','tidaknaik','santriwustha','nilaiaktif','tanggal','kelas','periode','nilaidiniyahsorted','nilaiumumsorted','nilaiMulokSorted','walikelas'));
+                $pdf->setPaper('legal');
+                return $pdf->stream('raport'.$santriwustha->namaLengkap.'.pdf');
+                return view('laporan/cetakUASGenapUula',compact('santriwustha','nilaiaktif','tanggal','kelas','periode','nilaidiniyahsorted','nilaiumumsorted','nilaiMulokSorted','walikelas'));
+                
+            }
+        }elseif($jenjang='smpPutra'){
+            if($periode='Ganjil'){
+
+            }else{
+
+            }
+            
+        }elseif($jenjang='smpPutri'){
+            if($periode='Ganjil'){
+
+            }else{
+
+            }
+            
+        }elseif($jenjang='smaPutra'){
+            if($periode='Ganjil'){
+
+            }else{
+
+            }
+
         }
         $pdf = PDF::loadview('laporan/cetaknilaisw',compact('santriwustha','periode','nilaiaktif','kelas','tanggal'));
         $pdf->setPaper('legal')->setOption('margin-left',20);
@@ -68,8 +157,6 @@ class CetakNilaiController extends Controller
         $kelas=kelas::where('id',$santriwustha->kelas_id)->first();
         $nilai=nilai::where('kelas_id',$santriwustha->kelas_id)->first();
         $periode=periode::where('status','Aktif')->first();
-        $mapelDiniyah=mapel::where('kategori','diniyah')
-                                ->where('jenjang',jenjang())->get();
         if($nilai!=null){
             $nilaiaktif=nilai::where('santriwustha_id',$santriwustha->id)
                                 ->where('kelas_id',$santriwustha->kelas_id)
@@ -111,11 +198,8 @@ class CetakNilaiController extends Controller
                 }));
             }
         }
-            // dd($nilaidiniyahsorted);
         $cekuser = auth()->user();
         $walikelas=waliKelas::where('user_id',$cekuser->id)->first();
-        // dd($nilaiMulokSorted);
-        // dd($cekuser->jenjang);
         if($cekuser->jenjang=="sd"){
             $pdf = PDF::loadview('laporan/cetaknilaimidUula',compact('santriwustha','nilaiaktif','tanggal','kelas','periode','nilaidiniyahsorted','nilaiumumsorted','nilaiMulokSorted','walikelas'));
             $pdf->setPaper('legal');
